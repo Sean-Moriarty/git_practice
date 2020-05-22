84bb4bc1b1e831aadcbf53ee2aa99026ceea0451
Sean Moriarty <hippiehaircut@gmail.com>
2020-05-21 18:12:58 -0700

    Just more junk on here to practice my Git skills

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hello world" * 5)
+print("hello world")
+# The only real point of this is to keep practicing Git in VSCode
+
+# 5/21/20
+my_string = "string variable"
+my_int = 42
+my_float = 3.14
+my_bool = True
+
+
